1d86454f66e80306957b665e839fdd408231577f
Ashhal Idrees <129538410+itsAshhal@users.noreply.github.com>
2024-09-03 18:26:25 +0500

    card+json
    updated

--- Assets/ScriptableObjects/AutomaticScripterForProductID.cs
+++ Assets/ScriptableObjects/AutomaticScripterForProductID.cs
@@ -30,26 +30,26 @@ public class AutomaticScripterForProductID : MonoBehaviour
             "8020886913191", // Big Dino
             "8015859384487", // Big Knife
             "8157975675047", // Chop
-            "8271000010919", // Daddy Chill
+           // "8271000010919", // Daddy Chill  ..
             "8030139875495", // Dracula's Dog
-            "8313349111975", // Duperman
+           // "8313349111975", // Duperman  ..
             "8029714677927", // Fearless Manta
-            "8290357117095", // Firehouse
+           // "8290357117095", // Firehouse  ..
             "8135873659047", // Gatorlord
             "8067560341671", // Gospel
             "8021966127271", // HardStop
             "7996384444583", // Honk
             "8029783687335", // Jarawangadananan
             "8170022371495", // Lunar Lightning
-            "8290356887719", // Mega Karen
+           // "8290356887719", // Mega Karen  ..
             "8162591539367", // Monkey Love
             "8237176422567", // Mundo
-            "8262740246695", // NDRYNK
-            "8289030897831", // Officer Mike Nasty
-            "8290356625575", // Post-Op
-            "8237189038247", // Protein Jake
+            //"8262740246695", // NDRYNK  ..
+            //"8289030897831", // Officer Mike Nasty  ..
+            //"8290356625575", // Post-Op  ..
+            //"8237189038247", // Protein Jake  ..
             "8163213017255", // Reptilian Murphy
-            "8263049281703", // Swifto
+            //"8263049281703", // Swifto  ..
             "8205446021287", // Theorem
             "8107594481831", // Trashcannibal
             "8210184732839", // Tumbo
